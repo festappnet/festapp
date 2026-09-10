45265d8da8d602d3fc2c2b043716a9aba980d90e
Michael Bujnovský <2659269+miakh@users.noreply.github.com>
2026-09-10 08:02:46 +0200

    fix: point hvezdamorska client sync at canonical endpoints

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -58,8 +58,8 @@ class AppConfig {
   static const String termsUrl = "https://hvezdamorska.festapp.net/terms/";
   static const String supportUrl = "https://hvezdamorska.festapp.net/support/";
   static const String deleteAccountUrl = "https://hvezdamorska.festapp.net/delete-account/";
-  static const String syncHeadOrigin = "";
-  static const String syncAssetOrigin = "";
+  static const String syncHeadOrigin = "https://sync.festapp.net";
+  static const String syncAssetOrigin = "https://assets.festapp.net";
   static const String imageApiUrl = 'https://image-api.festapp.net';
   static const String imageProjectId = 'a';
   static bool isProLicense = true;
