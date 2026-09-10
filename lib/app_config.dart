3e4cea961e2a74afaa872e17a8e072706d36043f
Michael Bujnovský <2659269+miakh@users.noreply.github.com>
2026-09-10 08:02:15 +0200

    fix: point absolventskyvelehrad client sync at canonical endpoints

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -58,8 +58,8 @@ class AppConfig {
   static const String termsUrl = "https://app.absolventskyvelehrad.cz/terms/";
   static const String supportUrl = "https://app.absolventskyvelehrad.cz/support/";
   static const String deleteAccountUrl = "https://app.absolventskyvelehrad.cz/delete-account/";
-  static const String syncHeadOrigin = "";
-  static const String syncAssetOrigin = "";
+  static const String syncHeadOrigin = "https://sync.festapp.net";
+  static const String syncAssetOrigin = "https://assets.festapp.net";
   static const String imageApiUrl = 'https://image-api.festapp.net';
   static const String imageProjectId = 'a';
   static bool isProLicense = true;
