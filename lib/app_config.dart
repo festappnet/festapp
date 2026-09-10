12841ced5f7eaad1b228c07cf5395cd3d0a430cd
Michael Bujnovský <2659269+miakh@users.noreply.github.com>
2026-09-10 08:02:34 +0200

    fix: point farnostopava client sync at canonical endpoints

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -58,8 +58,8 @@ class AppConfig {
   static const String termsUrl = "https://farnostopava.festapp.net/terms/";
   static const String supportUrl = "https://farnostopava.festapp.net/support/";
   static const String deleteAccountUrl = "https://farnostopava.festapp.net/delete-account/";
-  static const String syncHeadOrigin = "";
-  static const String syncAssetOrigin = "";
+  static const String syncHeadOrigin = "https://sync.festapp.net";
+  static const String syncAssetOrigin = "https://assets.festapp.net";
   static const String imageApiUrl = 'https://image-api.festapp.net';
   static const String imageProjectId = 'a';
   static bool isProLicense = true;
