2a94d26cc665500eb076bb10982e9ca399bfd7af
Michael Bujnovský <2659269+miakh@users.noreply.github.com>
2026-09-10 08:02:42 +0200

    fix: point festapptickets client sync at canonical endpoints

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -58,8 +58,8 @@ class AppConfig {
   static const String termsUrl = "https://vstupenky.online/terms/";
   static const String supportUrl = "https://vstupenky.online/support/";
   static const String deleteAccountUrl = "https://vstupenky.online/delete-account/";
-  static const String syncHeadOrigin = "";
-  static const String syncAssetOrigin = "";
+  static const String syncHeadOrigin = "https://sync.festapp.net";
+  static const String syncAssetOrigin = "https://assets.festapp.net";
   static const String imageApiUrl = 'https://image-api.festapp.net';
   static const String imageProjectId = 'default';
   static bool isProLicense = true;
